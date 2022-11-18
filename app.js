c44826353630d1c4f9f94c91ba13629f14c86332
Cnaresh <nareshchoudhary@NARESHs-MacBook-Pro.local>
2022-11-18 14:52:41 +0530

    level 4 bcrypt and salting 🔑

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -41,17 +41,21 @@ app.get("/register", function(req,res){
 
 
 app.post("/register", function(req,res){
-const newUser = new User({
-    email : req.body.username,
-    password : md5(req.body.password)
-});
-newUser.save(function(err){
-    if(err){
-        console.log(err);
-    }else{
-        res.render("secrets")
-    }
-})
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+        });
+
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets")
+            }
+        });
+    });
 });
 
 app.post("/login", function(req,res){
